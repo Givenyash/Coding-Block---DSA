d2ad1d479abdea366545ed5181e97ee9a65d8778
Givenyash <yash.ishu.barehi@gmail.com>
2026-01-20 15:14:16 +0530

    A program to print exceptional column asymmetric pattern

--- Lecture_3/7_pattern_ColumnAsymmetric_star_notSymbol.cpp
+++ Lecture_3/7_pattern_ColumnAsymmetric_star_notSymbol.cpp
@@ -6,29 +6,21 @@ int main ()
     cin>>n;
    
     for(int i=1;i<=n;i++){
+        //spaces
         for(int j=1; j<=n-i; j++){
             cout<<"  ";
         }
-        for(int j=1;j<=i;j++){ 
-            if(j%2!=0){
-                cout<<"* ";
-            }
-            else{
+        //Total pattern
+        for(int j=1;j<=2*i-1;j++){ 
+            if(j%2==0){
+                //exclamation mark
                 cout<<"! ";
             }
-            
-        }
-
-        for(int j=1;j<=i-1;j++){
-            if(j%2!=0){
-              cout<<"! ";  
-            }
             else{
-              cout<<"* "; 
+                //star 
+                cout<<"* ";
             }
-        }
-        for(int j=1; j<=n-i; j++){
-            cout<<"  ";
+            
         }
         cout<<endl;
     }
