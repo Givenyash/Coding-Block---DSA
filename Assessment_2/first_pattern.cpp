9835c2ad7bbf4f736c7888cc3b2cef0763449462
Givenyash <yash.ishu.barehi@gmail.com>
2026-01-27 00:56:47 +0530

    Pattern

--- Assessment_2/first_pattern.cpp
+++ Assessment_2/first_pattern.cpp
@@ -1,23 +1,62 @@
 //row Asymmetric
-//* * * * * 
-//* *   * * 
-//*       *
-//* *   * * 
-//* * * * * 
+// *	*	*	*	*	
+// *	*		*	*	
+// *				*	
+// *	*		*	*	
+// *	*	*	*	*
 
 
 #include<iostream>
 using namespace std;
 int main ()
 {
-    int n;
-    cin>>n;
-    for( int i=1; i<=n;i++){
-        for(int j=1;j<=n;j++){
-            cout<<"* ";
+    int m ,i;
+
+    cin>>m;
+	int n=(m+1)/2;
+
+    for(int i=1; i<=n; i++){
+        //star
+        for(int j=1; j<=n-i+1; j++){
+            cout<<"*"<<"\t";
         }
-        
+        //spaces
+        for(int j=1; j<=2*i-3; j++){
+            cout<<"\t";
+        }
+        if(i==1){
+            for(int j=1; j<=n-i; j++){
+                cout<<"*"<<"\t";
+            }
+        }
+        else{
+            for(int j=1; j<=n-i+1; j++){
+                cout<<"*"<<"\t";
+            }
+        }
         cout<<endl;
     }
-return 0;
+     for(int i=n-1; i>=1; i--){
+        //star
+        for(int j=1; j<=n-i+1; j++){
+            cout<<"*"<<"\t";
+        }
+        //spaces
+        for(int j=1; j<=2*i-3; j++){
+            cout<<"\t";
+        }
+        if(i==1){
+            for(int j=1; j<=n-i; j++){
+                cout<<"*"<<"\t";
+            }
+        }
+        else{
+            for(int j=1; j<=n-i+1; j++){
+                cout<<"*"<<"\t";
+            }
+        }
+        cout<<endl;
+    }
+    
+   return 0;
 }
