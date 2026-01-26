ebd3a89f61ad9aa076f2aac36eaeb554d23f0282
Givenyash <yash.ishu.barehi@gmail.com>
2026-01-26 23:42:19 +0530

    pattern

---
+++ Assessment_2/seventh_pattern.cpp
@@ -0,0 +1,27 @@
+//pattern with Zero
+// 1  
+// 2	2  
+// 3	0	3    
+// 4	0	0	4  
+// 5	0	0	0	5
+
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cin>>n;
+    
+   
+    for(int i=1;i<=n;i++){
+  		for(int j=1; j<= i; j++){
+           if(i ==j || j ==1)
+           cout<<i<<"\t";
+       else
+       	cout<< "0"<<"\t";
+        }
+  		cout<<endl;
+    } 
+   	
+   return 0;
+}
