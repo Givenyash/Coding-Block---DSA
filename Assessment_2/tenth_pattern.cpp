6c6bb9866dfc02991ac467b2cba268ac5f520650
Givenyash <yash.ishu.barehi@gmail.com>
2026-01-26 23:37:25 +0530

    pattern

---
+++ Assessment_2/tenth_pattern.cpp
@@ -0,0 +1,62 @@
+//Pattern - Magic
+// *********
+// **** ****
+// ***   ***
+// **     **
+// *       *
+// **     **
+// ***   ***
+// **** ****
+// ********* 
+
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n ,i;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        //star
+        for(int j=1; j<=n-i+1; j++){
+            cout<<"*";
+        }
+        //spaces
+        for(int j=1; j<=2*i-3; j++){
+            cout<<" ";
+        }
+        if(i==1){
+            for(int j=1; j<=n-i; j++){
+                cout<<"*";
+            }
+        }
+        else{
+            for(int j=1; j<=n-i+1; j++){
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+     for(int i=n-1; i>=1; i--){
+        //star
+        for(int j=1; j<=n-i+1; j++){
+            cout<<"*";
+        }
+        //spaces
+        for(int j=1; j<=2*i-3; j++){
+            cout<<" ";
+        }
+        if(i==1){
+            for(int j=1; j<=n-i; j++){
+                cout<<"*";
+            }
+        }
+        else{
+            for(int j=1; j<=n-i+1; j++){
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+    
+   return 0;
+}
