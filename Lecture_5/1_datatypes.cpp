b768b7ae38781e25b710f42c7db9f1175fc2f0d9
Givenyash <yash.ishu.barehi@gmail.com>
2026-01-28 23:31:03 +0530

    Datatype and function

---
+++ Lecture_5/1_datatypes.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    // long long int a = 311387;
+    // cout<<a*a<<endl;
+    // float b = 38.32;
+    // cout<<b*13.23<<endl;
+
+    int a=9 ;
+    string b="asihAOJASDH";
+    bool c="True";
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(b)<<endl;
+    cout<<sizeof(c);
+   return 0;
+}
