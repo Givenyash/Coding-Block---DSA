d0e2978a79212fdaf60f55892db4225e4c350c43
Givenyash <yash.ishu.barehi@gmail.com>
2026-01-30 14:35:01 +0530

    code updated//pattern

--- Lecture_3/5_pattern_column_Asymmetric.cpp
+++ Lecture_3/5_pattern_column_Asymmetric.cpp
@@ -16,9 +16,6 @@ int main ()
         for(int j=1;j<=i-1;j++){
             cout<<"* ";
         }
-        for(int j=1; j<=n-i; j++){
-            cout<<"  ";
-        }
         cout<<endl;
     }
    return 0;
