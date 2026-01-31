69593f421dfdd9c2a3cddf5b791072e0575964b5
Givenyash <yash.ishu.barehi@gmail.com>
2026-01-31 13:58:59 +0530

    Pattern

--- Practice_sheet_pattern.cpp/14_pattern.cpp
+++ Practice_sheet_pattern.cpp/14_pattern.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-
+    //upper
     for(int i = 1 ; i <= n; i++){
         for(int j =1 ; j<= n-i; j++){
             cout<<"  ";
@@ -23,7 +23,7 @@ int main(){
         }
         cout<<endl;
     }
-
+    //lower
     for(int i = n-1 ; i >= 1; i--){
         for(int j =1 ; j<= n-i; j++){
             cout<<"  ";
