6b6c4cd3bb633cab364d14a0ff2c5bc6d134bd47
Givenyash <yash.ishu.barehi@gmail.com>
2026-02-01 15:52:48 +0530

    number pattern

---
+++ Practice_sheet_pattern.cpp/23_number_pattern.cpp
@@ -0,0 +1,27 @@
+//                1
+//            1   1   1
+//        1   1   1   1   1   
+//    1   1   1   1   1   1   1
+// 1  1   1   1   1   1   1   1   1
+
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cin>>n;
+    
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
+            cout<<"  ";
+        }
+        for(int j=1; j<=i;j++){
+            cout<<"1 ";
+        }
+        for(int j=1; j<=i-1;j++){
+            cout<<"1 ";
+        }
+        cout<<endl;
+    }
+   return 0;
+}
