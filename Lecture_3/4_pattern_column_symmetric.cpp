0bb5dc068dc3584db5f228601dddddf3925ae4bb
Givenyash <yash.ishu.barehi@gmail.com>
2026-02-05 09:23:43 +0530

    column symmetry

--- Lecture_3/4_pattern_column_symmetric.cpp
+++ Lecture_3/4_pattern_column_symmetric.cpp
@@ -16,9 +16,7 @@ int main ()
         for(int j=1;j<=i;j++){
             cout<<"* ";
         }
-        for(int j=1; j<=n-i; j++){
-            cout<<"  ";
-        }
+        
         cout<<endl;
     }
    return 0;
