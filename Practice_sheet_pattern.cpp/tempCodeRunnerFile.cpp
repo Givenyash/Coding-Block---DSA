5f7ece79fd6d17f1aff905cd1a4ef4aa0b409b5b
Givenyash <yash.ishu.barehi@gmail.com>
2026-02-05 09:23:50 +0530

    pattern

---
+++ Practice_sheet_pattern.cpp/tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+for(int j= 1; j<=n-i-1; j++){
+    //         cout<<"  ";
+    //     }
+    //     //star
+    //     if(i==n){
+    //         for(int j= 1; j<= n-1; j++){
+    //             cout<<"* ";
+    //         }
+    //     }
+    //     else{
+    //         for(int j=1; j<=i; j++){
+    //         cout<<"* ";
+    //     }
+    //     }
+
+    //     cout<<endl;
+    // }
