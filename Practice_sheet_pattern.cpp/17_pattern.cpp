cd0467401ec58b8dd3a10d8e5ccd539b46aaeccf
Givenyash <yash.ishu.barehi@gmail.com>
2026-03-02 15:38:40 +0530

    changed

--- Practice_sheet_pattern.cpp/17_pattern.cpp
+++ Practice_sheet_pattern.cpp/17_pattern.cpp
@@ -21,7 +21,7 @@ int main ()
         for(int j=1 ; j<=i; j++){
             cout<<"  ";
         }
-        for(int j=1; j<=i; j++){
+        for(int j=1; j<=i-1; j++){
             cout<<"  ";
         }
         for(int j=1; j<=n-i; j++){
@@ -38,7 +38,7 @@ int main ()
         for(int j=1 ; j<=i; j++){
             cout<<"  ";
         }
-        for(int j=1; j<=i; j++){
+        for(int j=1; j<=i-1; j++){
             cout<<"  ";
         }
         for(int j=1; j<=n-i; j++){
