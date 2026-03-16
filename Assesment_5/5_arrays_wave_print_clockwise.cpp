63d3cbd1647be0509993291b2a6024a85bb2d934
Givenyash <yash.ishu.barehi@gmail.com>
2026-03-16 23:46:05 +0530

    wave print clock wise

---
+++ Assesment_5/5_arrays_wave_print_clockwise.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main ()
+{
+    int n,m; // n is for rows and m is for columns
+    cin>>n>>m;
+    vector<vector<int>> arr(n,vector<int> (m)); // initialized 2D vector
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>arr[i][j]; // input of 2D vector
+        }
+    }
+
+    // checking what is constant while traversing also what is traversing ?
+    // according to the Question
+    int left = 0;
+    int right = m-1;
+    int top = 0;
+    int bottom =n-1;
+    
+
+    while(left<=right and top<=bottom){
+        // left to right
+        for(int i=left; i<=right; i++){
+            //top
+            cout<<arr[top][i]<<", ";
+        }
+        top++;
+        // top to bottom
+        for(int i=top; i<=bottom; i++){
+            // right
+            cout<<arr[i][right]<<", ";
+        }
+        right--;
+
+        if(top<=bottom){
+            //right to left
+
+            for(int i= right ; i>=left; i--){
+                //bottom
+                cout<<arr[bottom][i]<<", ";
+            }
+            bottom--;
+        }
+        if(left<=right){
+            //bottom to top
+            for(int i= bottom; i>= top; i--){
+                cout<<arr[i][left]<<", ";
+            }
+            left++;
+        }
+    }
+	cout<<"END";
+   return 0;
+}
+
+// input
+// 4 4
+// 11 12 13 14
+// 21 22 23 24
+// 31 32 33 34
+// 41 42 43 44
+
+// output
+//11, 12, 13, 14, 24, 34, 44, 43, 42, 41, 31, 21, 22, 23, 33, 32, END
