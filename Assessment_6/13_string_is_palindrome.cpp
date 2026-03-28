83c9c6289484217f6923786469e8c6e676c987bc
Givenyash <yash.ishu.barehi@gmail.com>
2026-03-28 17:01:24 +0530

    string is palindrome?

---
+++ Assessment_6/13_string_is_palindrome.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size()-1 ;
+
+        while(left < right){
+          if( s[left] != s[right]){
+			  return false;
+		  }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+int main() {
+	string s;
+	cin>>s;
+
+	if(isPalindrome(s) ){
+		cout<<"true";
+	}
+	else{
+		cout<<"false";
+	}
+	return 0;
+}
+//input
+//abba
+
+//output
+//true
